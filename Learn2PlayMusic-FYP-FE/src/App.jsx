513dc7096c6ca39259937f350c369628540562e7
Shuoan Lee <leeshuoan38@gmail.com>
2023-02-01 23:45:16 +0800

    init base app

---
+++ Learn2PlayMusic-FYP-FE/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+import DefaultAppBar from './components/AppBar/DefaultAppBar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <DefaultAppBar />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <br /><br />
+
+    </div>
+  )
+}
+
+export default App
---
+++ Learn2PlayMusic-FYP-FE/src/components/AppBar/DefaultAppBar.jsx
@@ -0,0 +1,40 @@
+import Skeleton from '@mui/material/Skeleton';
+import { AppBar, Box, Toolbar, IconButton, Container, MenuItem, useTheme } from '@mui/material';
+
+function DefaultAppBar({ role, handleResetRoles }) {
+  const theme = useTheme()
+
+  return (
+    <>
+      {
+        <AppBar position="static" sx={{ bgcolor: theme.palette.background.paper }}>
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <IconButton disableRipple>
+                <img src="/SSSLogo1.png" width="70px" />
+              </IconButton>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                <MenuItem>
+                  <Skeleton variant="rounded" width={150} height={40} />
+                </MenuItem>
+                <MenuItem>
+                  <Skeleton variant="rounded" width={150} height={40} />
+                </MenuItem>
+                <MenuItem>
+                  <Skeleton variant="rounded" width={150} height={40} />
+                </MenuItem>
+              </Box>
+              <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                <MenuItem>
+                  <Skeleton variant="rounded" width={150} height={40} />
+                </MenuItem>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      }
+    </>
+  )
+}
+
+export default DefaultAppBar
