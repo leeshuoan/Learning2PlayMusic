ce4468e839e87cccb0e119190428e958d00a7b37
Aloysius <aloysiusng5@gmail.com>
2023-02-02 15:42:51 +0800

    updates

--- Learn2PlayMusic-FYP-FE/src/App.jsx
+++ Learn2PlayMusic-FYP-FE/src/App.jsx
@@ -1,23 +1,14 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import DefaultAppBar from "./components/AppBar/DefaultAppBar";
-=======
-import { useState } from 'react'
-import './App.css'
-import DefaultAppBar from './components/AppBar/DefaultAppBar'
 import ThemeProvider from './theme/index'
->>>>>>> c00274d846d41ce6bf5902c6bf5e09bae4cc5d97
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-<<<<<<< HEAD
-      <DefaultAppBar />
-=======
       <ThemeProvider>
         <DefaultAppBar />
         <br /><br />
@@ -39,7 +30,6 @@ function App() {
         <br /><br />
         <br /><br />
       </ThemeProvider>
->>>>>>> c00274d846d41ce6bf5902c6bf5e09bae4cc5d97
     </div>
   );
 }
