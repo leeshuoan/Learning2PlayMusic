c00274d846d41ce6bf5902c6bf5e09bae4cc5d97
Shuoan Lee <leeshuoan38@gmail.com>
2023-02-02 14:18:06 +0800

    made appbar responsive

--- Learn2PlayMusic-FYP-FE/src/components/AppBar/DefaultAppBar.jsx
+++ Learn2PlayMusic-FYP-FE/src/components/AppBar/DefaultAppBar.jsx
@@ -1,17 +1,27 @@
+import React from 'react';
 import Skeleton from '@mui/material/Skeleton';
-import { AppBar, Box, Toolbar, IconButton, Container, MenuItem, useTheme, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Box, Toolbar, IconButton, Menu, Container, MenuItem, useTheme, Typography } from '@mui/material';
 
 function DefaultAppBar({ role, handleResetRoles }) {
   const theme = useTheme()
-
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
   const pages = ['Our Website', 'Contact Us']
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   const handleRoute = (page) => {
     if (page === 'Our Website') {
       navigate(`https://www.learn2playmusic.sg/`)
     } else if (page === "Contact Us") {
       navigate(`https://www.learn2playmusic.sg/contact-us.html`)
-    } 
+    }
   }
 
   return (
@@ -22,8 +32,44 @@ function DefaultAppBar({ role, handleResetRoles }) {
             : role === "user" ? <UserAppBar handleResetRoles={handleResetRoles} />
               :
               <AppBar position="static" sx={{ bgcolor: theme.palette.background.paper }}>
-                <Container maxWidth="xl">
+                <Container maxWidth="xl" sx={{ width: 0.9 }}>
                   <Toolbar disableGutters style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                      <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleOpenNavMenu}
+                        color="inherit"
+                      >
+                        <MenuIcon />
+                      </IconButton>
+                      <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorElNav}
+                        anchorOrigin={{
+                          vertical: 'bottom',
+                          horizontal: 'left',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                          vertical: 'top',
+                          horizontal: 'left',
+                        }}
+                        open={Boolean(anchorElNav)}
+                        onClose={handleCloseNavMenu}
+                        sx={{
+                          display: { xs: 'block', md: 'none' },
+                        }}
+                      >
+                        {pages.map((page) => (
+                          <MenuItem key={page} onClick={() => handleRoute(page)}>
+                            <Typography textAlign="center" color="primary">{page}</Typography>
+                          </MenuItem>
+                        ))}
+                      </Menu>
+                    </Box>
                     <IconButton disableRipple>
                       <img src="/logo.png" width="150px" />
                     </IconButton>
