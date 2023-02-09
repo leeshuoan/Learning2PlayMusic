255596f9d0082e92f3882d881acc6dc8a4347d31
Aloysius <aloysiusng5@gmail.com>
2023-02-09 13:28:52 +0800

    basic functions for chat (read and send msg)

---
+++ LambdaFunctions/Chat/readChatMessage.py
@@ -0,0 +1,54 @@
+import boto3
+import json
+
+print('Loading function')
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Chat')
+    
+    conversationId = event.get('conversationId')
+    if not conversationId:
+        return {
+            'statusCode': 400,
+            'body': "Bad request, check your input"
+        }
+    
+    try:
+        response = table.scan(
+            FilterExpression=boto3.dynamodb.conditions.Attr("conversationId").eq(conversationId)
+        )        
+        res = response['Items']
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': "AWS resource error, contact admin"
+        }
+    
+    return {
+        'statusCode': 200,
+        'body': res
+    }
+
+print('Function done')
+
+"""
+sample response
+{
+    "statusCode": 200,
+    "body": [
+        {
+            "conversationId": "8cf0cd07-b89c-4140-88f0-327dbe720754+e67a3c45-e06e-4ca6-b57a-e149947b0223",
+            "messageBody": "hello from student",
+            "timestamp": "2023-02-09T04:33:23",
+            "author": "e67a3c45-e06e-4ca6-b57a-e149947b0223"
+        },
+        {
+            "conversationId": "8cf0cd07-b89c-4140-88f0-327dbe720754+e67a3c45-e06e-4ca6-b57a-e149947b0223",
+            "messageBody": "hello from teacher",
+            "timestamp": "2023-02-09T04:35:26",
+            "author": "8cf0cd07-b89c-4140-88f0-327dbe720754"
+        }
+    ]
+}
+"""
---
+++ LambdaFunctions/Chat/writeChatMessage.py
@@ -0,0 +1,52 @@
+import boto3
+import datetime
+
+def lambda_handler(event, context):
+    try:
+        dynamodb = boto3.client('dynamodb')
+        
+        conversationId = event.get('conversationId')
+        author = event.get('author')
+        messageBody = event.get('messageBody')
+        
+        # Validate input parameters
+        if not conversationId:
+            return {
+                'statusCode': 400,
+                'body': "Bad request, conversationId is required"
+            }
+        if not author:
+            return {
+                'statusCode': 400,
+                'body': "Bad request, author is required"
+            }
+        if not messageBody:
+            return {
+                'statusCode': 400,
+                'body': "Bad request, messageBody is required"
+            }
+        
+        # Generate unique timestamp
+        timestamp = str(datetime.datetime.now().isoformat())
+        
+        # Insert the new message into DynamoDB
+        response = dynamodb.put_item(
+            TableName='Chat',
+            Item={
+                'conversationId': {'S': conversationId},
+                'timestamp': {'S': timestamp},
+                'author': {'S': author},
+                'messageBody': {'S': messageBody}
+            }
+        )
+        
+        # Return success response
+        return {
+            'statusCode': 200,
+            'body': "Message sent successfully"
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': "Error sending message: " + str(e)
+        }
