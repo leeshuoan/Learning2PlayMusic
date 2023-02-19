7bb787c4d0780ea55bc2d0aa02711323ff1c0ae0
mintun-myo-2020 <mintun.myo.2020@scis.smu.edu.sg>
2023-02-19 17:05:43 +0800

    fix announcement folder

--- serverless/app.py
+++ serverless/app.py
@@ -11,7 +11,7 @@ from serverless.course_stack import CourseStack
 
 
 app = cdk.App()
-CourseStack(app, "ServerlessStack",
+CourseStack(app, "CourseStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
