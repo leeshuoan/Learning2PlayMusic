0bef4569680d990e4cf5b5ca5a77405888eef383
Aloysius <aloysiusng5@gmail.com>
2023-02-23 18:41:59 +0800

    chat cdk

--- serverless/serverless/chat_stack.py
+++ serverless/serverless/chat_stack.py
@@ -1,39 +1,81 @@
+import boto3
 from aws_cdk import core
+from aws_cdk import aws_iam
 from aws_cdk import aws_lambda as _lambda
 from aws_cdk import aws_apigateway as apigateway
-from aws_cdk import aws_dynamodb as dynamodb
+from aws_cdk import CfnOutput
 
-class MyServerlessAppStack(core.Stack):
+class ChatStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # create a DynamoDB table
-        table = dynamodb.Table(self, "myTable",
-                               partition_key=dynamodb.Attribute(
-                                   name="id", type=dynamodb.AttributeType.STRING))
+        FUNCTIONS_FOLDER = "./lambda_functions/"
+        CHAT_FUNCTIONS_FOLDER = "chat"
 
-        # create a Lambda function
-        handler = _lambda.Function(self, "myLambda",
-                                    runtime=_lambda.Runtime.PYTHON_3_8,
-                                    code=_lambda.Code.from_asset("lambda"),
-                                    handler="main.handler",
-                                    environment={
-                                        'TABLE_NAME': table.table_name
-                                    })
+        # Get existing iam role (lambda-general-role) ============================================================
+        iam = boto3.client("iam")
+        role = iam.get_role(RoleName="lambda-general-role")
+        role_arn = role["Role"]["Arn"]
+        LAMBDA_ROLE = aws_iam.Role.from_role_arn(
+            self, "lambda-general-role", role_arn)
 
-        # grant the Lambda function permissions to access the DynamoDB table
-        table.grant_read_write_data(handler)
+        # create a Lambda function  ============================================================
+        get_contacts = _lambda.Function(self, "getContacts", runtime=_lambda.Runtime.PYTHON_3_9, handler=f"{CHAT_FUNCTIONS_FOLDER}.getContacts.lambda_handler", code=_lambda.Code.from_asset(FUNCTIONS_FOLDER), role=LAMBDA_ROLE)
+        getConversations = _lambda.Function(self, "getConversations", runtime=_lambda.Runtime.PYTHON_3_9, handler=f"{CHAT_FUNCTIONS_FOLDER}.getConversations.lambda_handler", code=_lambda.Code.from_asset(FUNCTIONS_FOLDER), role=LAMBDA_ROLE)
+        readConversation = _lambda.Function(self, "readConversation", runtime=_lambda.Runtime.PYTHON_3_9, handler=f"{CHAT_FUNCTIONS_FOLDER}.readConversation.lambda_handler", code=_lambda.Code.from_asset(FUNCTIONS_FOLDER), role=LAMBDA_ROLE)
+        sendMessage = _lambda.Function(self, "sendMessage", runtime=_lambda.Runtime.PYTHON_3_9, handler=f"{CHAT_FUNCTIONS_FOLDER}.sendMessage.lambda_handler", code=_lambda.Code.from_asset(FUNCTIONS_FOLDER), role=LAMBDA_ROLE)
 
-        # create an API Gateway REST API
-        rest_api = apigateway.RestApi(self, "myRestApi")
+        # create an API Gateway REST API ==========================================================================================
+        rest_api = apigateway.RestApi(self, "Chat", description="chat APIs")
 
-        # create a resource and method for the API
-        resource = rest_api.root.add_resource("items")
-        get_method = resource.add_method("GET", apigateway.LambdaIntegration(handler))
+        # create a resource and method for /chat/getContacts
+        contacts_resource = rest_api.root.add_resource("chat").add_resource("getContacts")
+        contacts_method = contacts_resource.add_method("GET", apigateway.LambdaIntegration(get_contacts),
+                                                       request_parameters={
+                                                           'method.request.querystring.userId': True
+                                                       })
+        # create a resource and method for /chat/getConversations
+        conversations_resource = rest_api.root.add_resource("chat").add_resource("getConversations")
+        conversations_method = conversations_resource.add_method("GET", apigateway.LambdaIntegration(getConversations),
+                                                                  request_parameters={
+                                                                      'method.request.querystring.userId': True
+                                                                  })
+        # create a resource and method for /chat/readConversation
+        read_resource = rest_api.root.add_resource("chat").add_resource("readConversation")
+        read_method = read_resource.add_method("GET", apigateway.LambdaIntegration(readConversation),
+                                                request_parameters={
+                                                    'method.request.querystring.conversationId': True
+                                                })
 
-        # add a request validator to the GET method
-        get_method.request_validator = apigateway.RequestValidator(
-            self, "myRequestValidator",
-            validate_request_body=True,
-            validate_request_parameters=True
+        # create a resource and method for /chat/sendMessage
+        send_message_resource = rest_api.root.add_resource("chat").add_resource("sendMessage")
+        send_message_method = send_message_resource.add_method("POST", apigateway.LambdaIntegration(handler))
+
+        # add a request validator to the GET methods
+        for method in [contacts_method, conversations_method, read_method]:
+            method.request_validator = apigateway.RequestValidator(
+                self, "myRequestValidator",
+                validate_request_parameters=True
+            )
+
+        # Enable CORS for each resource/sub-resource etc.
+        contacts_resource.add_cors_preflight(
+            allow_origins=["*"], allow_methods=["GET", "POST", "DELETE"], status_code=200)
+        conversations_method.add_cors_preflight(
+            allow_origins=["*"], allow_methods=["GET", "PUT", "DELETE"], status_code=200)
+        read_resource.add_cors_preflight(
+            allow_origins=["*"], allow_methods=["GET", "PUT", "DELETE"], status_code=200)
+        send_message_resource.add_cors_preflight(
+            allow_origins=["*"], allow_methods=["POST"], status_code=200)
+        
+        # Export API gateway to use in other Stacks
+        CfnOutput(
+            self, 'MyApiIdOutput',
+            value=rest_api.rest_api_id,
+            export_name='mainApiId',
+        )
+        CfnOutput(
+            self, 'MyApiRootResourceIdOutput',
+            value=rest_api.root.resource_id,
+            export_name='mainApiRootResourceIdOutput',
         )
