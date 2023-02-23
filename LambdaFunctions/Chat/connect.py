b587fb8b21d19ca31631e950f934d3fd35dab80b
Aloysius <aloysiusng5@gmail.com>
2023-02-23 14:51:38 +0800

    chat

--- LambdaFunctions/Chat/connect.py
+++ LambdaFunctions/Chat/connect.py
@@ -9,21 +9,21 @@ table = dynamodb.Table('connectionMappings')
 def lambda_handler(event, context):
     try:
         # Get the user ID and connection ID from the WebSocket event
-        user_id = event['requestContext']['authorizer']['user_id']
+        userId = event['requestContext']['authorizer']['userId']
         connection_id = event['requestContext']['connectionId']
         
         # Check if the user ID already exists in the connection mappings table
-        response = table.get_item(Key={'user_id': user_id})
+        response = table.get_item(Key={'userId': userId})
         item = response.get('Item')
         
         # If the user ID already exists, delete the existing mapping
         if item:
             existing_connection_id = item.get('connection_id')
             if existing_connection_id != connection_id:
-                table.delete_item(Key={'user_id': user_id})
+                table.delete_item(Key={'userId': userId})
         
         # Store the new connection ID in the connection mappings table
-        table.put_item(Item={'user_id': user_id, 'connection_id': connection_id})
+        table.put_item(Item={'userId': userId, 'connection_id': connection_id})
         
         # Return a successful response
         return {
