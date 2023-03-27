ad2e3175f27c29b1a8b0e5b3425be93d1fb58936
Aloysius <aloysiusng5@gmail.com>
2023-03-27 14:43:57 +0800

    update return message for un-enrol user

--- serverless/lambda_functions/user/course/delete_user_course.py
+++ serverless/lambda_functions/user/course/delete_user_course.py
@@ -1,53 +1,52 @@
 import sys
-import boto3
-import json
-import uuid
 
-from global_functions.responses import *
-from global_functions.exists_in_db import *
+import boto3
 from global_functions.cognito import *
+from global_functions.exists_in_db import *
+from global_functions.responses import *
+
 
 def lambda_handler(event, context):
 
     try:
-
-        userId = event['queryStringParameters']['userId']
+        userId = event["queryStringParameters"]["userId"]
 
         # check if user exists in Cognito
         user = get_user(userId)
         if not user:
-            return response_404('userId does not exist in Cognito')
+            return response_404("userId does not exist in Cognito")
 
         # check if <courseId> exists in database
-        courseId = event['queryStringParameters']['courseId']
+        courseId = event["queryStringParameters"]["courseId"]
         if not id_exists("Course", "Course", courseId):
             return response_404("courseId does not exist in database.")
 
-        if 'studentId' in user:
-            userType = 'Student'
+        if "studentId" in user:
+            userType = "Student"
 
-        elif 'teacherId' in user:
-            userType = 'Teacher'
+        elif "teacherId" in user:
+            userType = "Teacher"
 
         else:
-            return response_400("Please check that you have entered a correct studentId/teacherId")
+            return response_400(
+                "Please check that you have entered a correct studentId/teacherId"
+            )
 
         # check if <userId><courseId> combination exists in database
         if not combination_id_exists(userType, userId, "Course", courseId):
-            return response_202_msg(f"This {userType.lower()} has not been enrolled with the course")
+            return response_202_msg(
+                f"This {userType.lower()} has not been enrolled with the course"
+            )
 
         # remove a user from course
         dynamodb = boto3.resource("dynamodb")
         table = dynamodb.Table("LMS")
 
         table.delete_item(
-            Key= {
-                "PK": f"{userType}#{userId}",
-                "SK": f"Course#{courseId}"
-            }
-            )
+            Key={"PK": f"{userType}#{userId}", "SK": f"Course#{courseId}"}
+        )
 
-        return response_200_msg("successfully deleted item")
+        return response_200_msg("successfully un-enrolled user from course ")
 
     except Exception as e:
         # print(f".......... 🚫 UNSUCCESSFUL: Failed request for Course ID: {courseId} 🚫 ..........")
