90f5483454dfd9f041fe1d06b7a363cacd7e3fe4
Aloysius <aloysiusng5@gmail.com>
2023-03-29 11:58:34 +0800

    remove jwt from getcourse_quiz.py

--- serverless/lambda_functions/course_quiz/get_course_quiz_withJWT.py
+++ serverless/lambda_functions/course_quiz/get_course_quiz_withJWT.py
@@ -2,32 +2,42 @@ import sys
 import boto3
 import json
 import decimal
+import jwt
 
 from global_functions.responses import *
 
+
 class Encoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, decimal.Decimal): return float(obj)
+        if isinstance(obj, decimal.Decimal):
+            return float(obj)
 
 # Get all quizzes by courseid
+
 
 def lambda_handler(event, context):
 
     queryStringParameters: dict = event["queryStringParameters"]
+    headers: dict = event["headers"]
+    authorization_header = headers.get("Authorization")
+    if authorization_header:
+        token = authorization_header.split(" ")[-1]
+
     res = {}
     try:
         dynamodb = boto3.resource("dynamodb")
         table = dynamodb.Table("LMS")
-        
+
         courseId = queryStringParameters["courseId"]
 
         if "studentId" not in queryStringParameters.keys():
-            items = handle_general_course_quiz(courseId, table, queryStringParameters)
+            items = handle_general_course_quiz(
+                courseId, table, queryStringParameters, token)
 
         else:
             studentId = queryStringParameters["studentId"]
-            items = handle_student_course_quiz(courseId, studentId, table, queryStringParameters)
-
+            items = handle_student_course_quiz(
+                courseId, studentId, table, queryStringParameters, token)
 
         res["statusCode"] = 200
         res["headers"] = {
@@ -35,18 +45,16 @@ def lambda_handler(event, context):
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "POST,GET,PUT"
         }
-        res["body"] = json.dumps(items, cls = Encoder)
+        res["body"] = json.dumps(items, cls=Encoder)
 
         return res
-    	
 
     except Exception as e:
-    	# print(f".......... 🚫 UNSUCCESSFUL: Failed request for Course ID: {courseId} 🚫 ..........")
+        # print(f".......... 🚫 UNSUCCESSFUL: Failed request for Course ID: {courseId} 🚫 ..........")
         exception_type, exception_object, exception_traceback = sys.exc_info()
-        filename = exception_traceback.tb_frame.f_code.co_filename
+
         line_number = exception_traceback.tb_lineno
         print("❗Exception type: ", exception_type)
-        print("❗File name: ", filename)
         print("❗Line number: ", line_number)
         print("❗Error: ", e)
         if exception_type == KeyError:
@@ -54,7 +62,7 @@ def lambda_handler(event, context):
         return response_500((str(exception_type) + str(e)))
 
 
-def handle_general_course_quiz(courseId, table, queryStringParameters):
+def handle_general_course_quiz(courseId, table, queryStringParameters, token):
     if "quizId" in queryStringParameters.keys():
         quizId = queryStringParameters["quizId"]
         response = table.get_item(
@@ -63,21 +71,21 @@ def handle_general_course_quiz(courseId, table, queryStringParameters):
                 "SK": f"Quiz#{quizId}"
             })
         items = response["Item"]
-        
+
     else:
+        filter_expression, expression_values = generate_expression_attribute_values(token, courseId)
         response = table.query(
             KeyConditionExpression="PK= :PK AND begins_with(SK, :SK)",
-            ExpressionAttributeValues={
-                ":PK": f"Course#{courseId}",
-                ":SK": f"Quiz#"
-            },
-            FilterExpression='attribute_not_exists(QuestionOptionType)'
+            FilterExpression=filter_expression,
+            ExpressionAttributeValues=expression_values
+
         )
         items = response["Items"]
 
     return items
 
-def handle_student_course_quiz(courseId, studentId, table, queryStringParameters):
+
+def handle_student_course_quiz(courseId, studentId, table, queryStringParameters, token):
     if "quizId" in queryStringParameters.keys():
         quizId = queryStringParameters["quizId"]
         response = table.get_item(
@@ -86,16 +94,30 @@ def handle_student_course_quiz(courseId, studentId, table, queryStringParameters
                 "SK": f"Student#{studentId}Quiz#{quizId}"
             })
         items = response["Item"]
-        
+
     else:
+        filter_expression, expression_values = generate_expression_attribute_values(token, courseId)
         response = table.query(
             KeyConditionExpression="PK= :PK AND begins_with(SK, :SK)",
-            ExpressionAttributeValues={
-                ":PK": f"Course#{courseId}",
-                ":SK": f"Student#{studentId}Quiz#"
-            },
-            FilterExpression='attribute_not_exists(QuestionOptionType)'
+            FilterExpression=filter_expression,
+            ExpressionAttributeValues=expression_values
         )
         items = response["Items"]
 
     return items
+
+
+def generate_expression_attribute_values(token, courseId):
+    # jwt_payload = jwt.decode(token, verify=False, algorithms=["RS256"])
+    jwt_payload = jwt.decode(token, options={"verify_signature": False})
+    if jwt_payload["custom:role"] == "User":
+        return ['Visibility= :visibility', {
+            ":PK": f"Course#{courseId}",
+            ":SK": f"Quiz#",
+            ":visibility": True
+        }]
+    else:
+        return ["", {
+            ":PK": f"Course#{courseId}",
+            ":SK": f"Quiz#"
+        }]
