b680e4a28361c7936c8aa175cf8d48bc43397c00
Aloysius <aloysiusng5@gmail.com>
2023-04-02 18:10:04 +0800

    error message for userHome and fix deleete course

--- serverless/lambda_functions/course/delete_course.py
+++ serverless/lambda_functions/course/delete_course.py
@@ -1,9 +1,10 @@
 import sys
-import boto3
-import json
 
-from global_functions.responses import *
+import boto3
+from boto3.dynamodb.conditions import Key
 from global_functions.exists_in_db import *
+from global_functions.responses import *
+
 
 def lambda_handler(event, context):
 
@@ -11,22 +12,25 @@ def lambda_handler(event, context):
 
         # VALIDATION
         # check if <courseId> exists in database
-        courseId = event['queryStringParameters']['courseId']
+        courseId = event["queryStringParameters"]["courseId"]
         if not id_exists("Course", "Course", courseId):
             return response_404("courseId does not exist in database")
 
         dynamodb = boto3.resource("dynamodb")
         table = dynamodb.Table("LMS")
 
-        response = table.delete_item(
-            Key= {
-                "PK": "Course",
-                "SK": f"Course#{event['queryStringParameters']['courseId']}"
-            }
-            )
+        # Query the database for all items related to the courseId
+        response = table.query(
+            KeyConditionExpression=Key("PK").contains(f"Course#{courseId}")
+            & Key("SK").contains(f"Course#{courseId}")
+        )
+
+        with table.batch_writer() as batch:
+            # Delete all items related to the courseId in the database
+            for item in response["Items"]:
+                batch.delete_item(Key=item)
 
         return response_200_msg("successfully deleted item")
-
 
     except Exception as e:
         # print(f".......... 🚫 UNSUCCESSFUL: Failed request for Course ID: {courseId} 🚫 ..........")
