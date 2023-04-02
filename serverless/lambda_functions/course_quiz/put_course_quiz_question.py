ab5828dca0ad8f1c3fb56e0a1ed45c0a683f1f5d
mintun-myo-2020 <mintun.myo.2020@scis.smu.edu.sg>
2023-04-02 17:03:37 +0800

    change to **

--- serverless/lambda_functions/course_quiz/put_course_quiz_question.py
+++ serverless/lambda_functions/course_quiz/put_course_quiz_question.py
@@ -56,11 +56,9 @@ def lambda_handler(event, context):
             "Options": options,
             "Answer": answer,
             "Attempts": 0,
-            "Correct": 0
+            "Correct": 0,
+            **options_stats
         }
-        for option in options:
-            item[option] = {str(0)}
-            
         if s3_params is not None:
             item["QuestionImage"] = "{0}/{1}".format(s3_params["Bucket"], s3_params["Key"])
 
