00d2cb280c718357db5440ba44df6717fed9d0da
Aloysius <aloysiusng5@gmail.com>
2023-04-02 14:20:04 +0800

    fixed unauthorised page

--- Learn2PlayMusic-FYP-FE/src/components/Unauthorized.jsx
+++ Learn2PlayMusic-FYP-FE/src/components/Unauthorized.jsx
@@ -1,25 +1,34 @@
-import { Typography, Link } from "@mui/material";
+import { Link, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-const Unauthorized = ({userRole}) => {
-  let redirectLink = ""
+const Unauthorized = ({ userRole }) => {
+  let redirectLink = "";
+  const navigate = useNavigate();
 
-  console.log(userRole)
+  console.log(userRole);
   if (userRole === "Admin") {
-    redirectLink = "admin"
+    redirectLink = "admin";
   } else if (userRole === "Teacher") {
-    redirectLink = "teacher"
+    redirectLink = "teacher";
   } else if (userRole === "Teacher") {
-    redirectLink = "teacher"
+    redirectLink = "teacher";
   }
 
-    return (
-      <div>
-        <Typography variant="h4" sx={{ pt:3, textAlign:"center" }}>You are not authorized to visit this page</Typography>
-        <Typography sx={{ pt:1, textAlign:"center" }}>
-        <Link onClick={() => { window.location.href =  redirectLink}}>Go to the home page</Link>
-        </Typography>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Typography variant="h4" sx={{ pt: 3, textAlign: "center" }}>
+        You are not authorized to visit this page
+      </Typography>
+      <Typography sx={{ pt: 1, textAlign: "center" }}>
+        <Link
+          onClick={() => {
+            navigate("/");
+          }}>
+          Go to the home page
+        </Link>
+      </Typography>
+    </div>
+  );
+};
 
 export default Unauthorized;
