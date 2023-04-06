3fe1bf8fafbc51fc0bb000136463ee2fe56fa043
Aloysius <aloysiusng5@gmail.com>
2023-04-06 14:31:54 +0800

    smaller modal size for delete

--- Learn2PlayMusic-FYP-FE/src/components/SuperAdmin/Course/SuperAdminCourseManagement.jsx
+++ Learn2PlayMusic-FYP-FE/src/components/SuperAdmin/Course/SuperAdminCourseManagement.jsx
@@ -15,7 +15,7 @@ const SuperAdminCourseManagement = () => {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: "50%",
+    width: "30%",
     bgcolor: "background.paper",
     border: "1px solid #000",
     borderRadius: 2,
